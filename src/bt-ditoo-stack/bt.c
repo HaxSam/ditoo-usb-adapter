1ada8c005734089b5c82b177203836fa5eafe576
haxsam <haxsam@pm.me>
2025-05-23 22:39:06 +0200

    first bluetooth control works
    time to combine usb cdc and bluetooth

--- src/bt-ditoo-stack/bt.c
+++ src/bt-ditoo-stack/bt.c
@@ -1,61 +1,74 @@
 #include "bt.h"
 
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // bluetooth stack
 #include "btstack.h"
 #include "btstack_run_loop.h"
+#include "hci_cmd.h"
 
 // Pico
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 
-#define LED_QUICK_FLASH_DELAY_MS 100
-#define LED_SLOW_FLASH_DELAY_MS 1000
-
 typedef enum {
-    TC_OFF,
-    TC_IDLE,
-    TC_W4_SCAN_RESULT,
-    TC_W4_CONNECT,
-    TC_W4_SERVICE_RESULT,
-    TC_W4_CHARACTERISTIC_RESULT,
-    TC_W4_ENABLE_NOTIFICATIONS_COMPLETE,
-    TC_W4_READY
-} gc_state_t;
+    IDLE,
+    W4_SCAN_RESULTS,
+    W4_SCAN_COMPLETE,
+    W4_CONNECT,
+    W4_SDP_RESULT,
+    W2_SEND_SDP_QUERY,
+    W4_RFCOMM_CHANNEL,
+    SENDING,
+    DONE
+} state_t;
 
-static btstack_packet_callback_registration_t hci_event_callback_registration;
-static gc_state_t state = TC_OFF;
-static bd_addr_t server_addr;
+static bd_addr_t server_addr = {0xB1, 0x21, 0x81, 0x64, 0xE2, 0x9E};
 static bd_addr_type_t server_addr_type;
 static hci_con_handle_t connection_handle;
-static gatt_client_service_t server_service;
-static gatt_client_characteristic_t server_characteristic;
-static bool listener_registered;
-static gatt_client_notification_t notification_listener;
-static btstack_timer_source_t heartbeat;
+static uint8_t rfcomm_server_channel;
 
-static void hci_event_handler(uint8_t packet_type, uint16_t channel, uint8_t* packet, uint16_t size);
-static void heartbeat_handler(struct btstack_timer_source* ts);
+uint8_t data[] = {0x01, 0x04, 0x00, 0x74, 0x10, 0x88, 0x00, 0x02};
+
+static state_t state = IDLE;
+static uint16_t rfcomm_cid = 0;
+static uint16_t rfcomm_mtu;
+
+// Handler
+static btstack_timer_source_t heartbeat;
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+static btstack_context_callback_registration_t handle_sdp_client_query_request;
+
+// Handler methods
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
+static void hci_packet_handler(uint8_t *packet, uint16_t size);
+static void handle_start_sdp_client_query(void *context);
+static void handle_query_rfcomm_event(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
+static void rfcomm_packet_handler(uint8_t *packet, uint16_t size);
+static void heart_beat_handler(btstack_timer_source_t *ts);
+
+// Helper methods
+static bool advertisement_report_contains_device_name(char *search_name, uint8_t *advertisement_report);
 
 //--------------------------------------------------------------------+
 // Main
 //--------------------------------------------------------------------+
 
-void bt_client_task(__unused void* param) {
+void bt_client_task(void *param) {
+    UNUSED(param);
     hard_assert(cyw43_arch_init() == PICO_OK);
 
     l2cap_init();
-    sm_init();
-    sm_set_io_capabilities(IO_CAPABILITY_NO_INPUT_NO_OUTPUT);
-    gatt_client_init();
+    rfcomm_init();
 
-    hci_event_callback_registration.callback = &hci_event_handler;
+    hci_event_callback_registration.callback = &packet_handler;
     hci_add_event_handler(&hci_event_callback_registration);
 
-    heartbeat.process = &heartbeat_handler;
-    btstack_run_loop_set_timer(&heartbeat, LED_SLOW_FLASH_DELAY_MS);
-    btstack_run_loop_add_timer(&heartbeat);
+    // SDP init
+    gap_ssp_set_io_capability(SSP_IO_CAPABILITY_DISPLAY_YES_NO);
 
     hci_power_control(HCI_POWER_ON);
 
@@ -64,24 +77,147 @@ void bt_client_task(__unused void* param) {
     }
 }
 
-static void client_start(void) {
-    printf("Start scanning!\n");
-    state = TC_W4_SCAN_RESULT;
+static void start_scan() {
+    printf("Starting scanning!\n");
+    state = W4_SCAN_RESULTS;
     gap_set_scan_parameters(1, 0x0030, 0x0030);
     gap_start_scan();
+}
+
+static void stop_scan() {
+    printf("Stop scanning!\n");
+    state = W4_SCAN_COMPLETE;
+    gap_stop_scan();
 }
 
 //--------------------------------------------------------------------+
 // Bluetooth handler
 //--------------------------------------------------------------------+
 
-static bool advertisement_report_contains_device_name(char* search_name, uint8_t* advertisement_report) {
-    // get advertisement from report event
-    const uint8_t* adv_data = gap_event_advertising_report_get_data(advertisement_report);
-    uint8_t adv_len = gap_event_advertising_report_get_data_length(advertisement_report);
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size) {
+    UNUSED(channel);
 
-    bd_addr_t address;
-    gap_event_advertising_report_get_address(advertisement_report, address);
+    switch (packet_type) {
+        case HCI_EVENT_PACKET:
+            hci_packet_handler(packet, size);
+            break;
+        case RFCOMM_DATA_PACKET:
+            rfcomm_packet_handler(packet, size);
+            break;
+        default:
+            break;
+    }
+}
+
+static void hci_packet_handler(uint8_t *packet, uint16_t size) {
+    UNUSED(size);
+
+    uint8_t event = hci_event_packet_get_type(packet);
+
+    switch (event) {
+        case BTSTACK_EVENT_STATE:
+            // BTstack activated, get started
+            if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
+            start_scan();
+            break;
+
+        case GAP_EVENT_ADVERTISING_REPORT:
+            if (state != W4_SCAN_RESULTS) return;
+
+            if (!advertisement_report_contains_device_name("DitooPro", packet)) return;
+            gap_event_advertising_report_get_address(packet, server_addr);
+            server_addr_type = gap_event_advertising_report_get_address_type(packet);
+
+            printf("Found DitooPro on %s!\n", bd_addr_to_str(server_addr));
+            stop_scan();
+
+            handle_sdp_client_query_request.callback = &handle_start_sdp_client_query;
+            (void)sdp_client_register_query_callback(&handle_sdp_client_query_request);
+
+            break;
+
+        case RFCOMM_EVENT_CHANNEL_OPENED:
+            if (rfcomm_event_channel_opened_get_status(packet)) {
+                printf("RFCOMM channel open failed, status 0x%02x\n", rfcomm_event_channel_opened_get_status(packet));
+                return;
+            };
+            rfcomm_cid = rfcomm_event_channel_opened_get_rfcomm_cid(packet);
+            rfcomm_mtu = rfcomm_event_channel_opened_get_max_frame_size(packet);
+            printf("RFCOMM channel open succeeded. New RFCOMM Channel ID 0x%02x, max frame size %u\n", rfcomm_cid, rfcomm_mtu);
+
+            btstack_run_loop_set_timer_handler(&heartbeat, heart_beat_handler);
+            btstack_run_loop_set_timer(&heartbeat, 1000);
+            btstack_run_loop_add_timer(&heartbeat);
+            break;
+
+        case RFCOMM_EVENT_CAN_SEND_NOW:
+            rfcomm_send(rfcomm_cid, data, 8);
+            break;
+
+        case RFCOMM_EVENT_CHANNEL_CLOSED:
+            printf("RFCOMM channel closed\n");
+            rfcomm_cid = 0;
+            btstack_run_loop_remove_timer(&heartbeat);
+            break;
+
+        default:
+            break;
+    }
+}
+
+static void handle_start_sdp_client_query(void *context) {
+    UNUSED(context);
+    state = W4_RFCOMM_CHANNEL;
+    sdp_client_query_rfcomm_channel_and_name_for_uuid(&handle_query_rfcomm_event, server_addr, BLUETOOTH_SERVICE_CLASS_SERIAL_PORT);
+}
+
+static void handle_query_rfcomm_event(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size) {
+    UNUSED(packet_type);
+    UNUSED(channel);
+    UNUSED(size);
+
+    switch (hci_event_packet_get_type(packet)) {
+        case SDP_EVENT_QUERY_RFCOMM_SERVICE:
+            rfcomm_server_channel = sdp_event_query_rfcomm_service_get_rfcomm_channel(packet);
+            break;
+        case SDP_EVENT_QUERY_COMPLETE:
+            if (sdp_event_query_complete_get_status(packet)) {
+                printf("SDP query failed, status 0x%02x\n", sdp_event_query_complete_get_status(packet));
+                break;
+            }
+            if (rfcomm_server_channel == 0) {
+                printf("No SPP service found\n");
+                break;
+            }
+            printf("SDP query done, channel 0x%02x.\n", rfcomm_server_channel);
+            printf("Connecting to device with addr %s.\n", bd_addr_to_str(server_addr));
+            rfcomm_create_channel(packet_handler, server_addr, rfcomm_server_channel, NULL);
+            break;
+        default:
+            break;
+    }
+}
+
+static void rfcomm_packet_handler(uint8_t *packet, uint16_t size) {
+    printf("Data recived (size: %d): '%.*s'\n", size, size, packet);
+}
+
+static void heart_beat_handler(btstack_timer_source_t *ts) {
+    if (rfcomm_cid) {
+        rfcomm_request_can_send_now_event(rfcomm_cid);
+    }
+    btstack_run_loop_set_timer(ts, 1000);
+    btstack_run_loop_add_timer(ts);
+}
+
+//--------------------------------------------------------------------+
+// Helper methods
+//--------------------------------------------------------------------+
+
+static bool advertisement_report_contains_device_name(char *search_name, uint8_t *advertisement_report) {
+    // get advertisement from report event
+    const uint8_t *adv_data = gap_event_advertising_report_get_data(advertisement_report);
+    uint8_t adv_len = gap_event_advertising_report_get_data_length(advertisement_report);
 
     char device_name[31];
     device_name[0] = '\0';
@@ -91,7 +227,7 @@ static bool advertisement_report_contains_device_name(char* search_name, uint8_t
     for (ad_iterator_init(&context, adv_len, adv_data); ad_iterator_has_more(&context); ad_iterator_next(&context)) {
         uint8_t data_type = ad_iterator_get_data_type(&context);
         uint8_t data_size = ad_iterator_get_data_len(&context);
-        const uint8_t* data = ad_iterator_get_data(&context);
+        const uint8_t *data = ad_iterator_get_data(&context);
 
         switch (data_type) {
             case BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME:
@@ -106,170 +242,4 @@ static bool advertisement_report_contains_device_name(char* search_name, uint8_t
     }
 
     return strncmp(device_name, search_name, strlen(search_name)) == 0;
-}
-
-static void handle_gatt_client_event(__unused uint8_t packet_type, __unused uint16_t channel, __unused uint8_t* packet, uint16_t size) {
-    uint8_t att_status;
-    switch (state) {
-        case TC_W4_SERVICE_RESULT:
-            switch (hci_event_packet_get_type(packet)) {
-                case GATT_EVENT_SERVICE_QUERY_RESULT:
-                    // store service (we expect only one)
-                    printf("Storing service\n");
-                    gatt_event_service_query_result_get_service(packet, &server_service);
-                    break;
-                case GATT_EVENT_QUERY_COMPLETE:
-                    att_status = gatt_event_query_complete_get_att_status(packet);
-                    if (att_status != ATT_ERROR_SUCCESS) {
-                        printf("SERVICE_QUERY_RESULT, ATT Error 0x%02x.\n", att_status);
-                        gap_disconnect(connection_handle);
-                        break;
-                    }
-                    // service query complete, look for characteristic
-                    state = TC_W4_CHARACTERISTIC_RESULT;
-                    printf("Search for env sensing characteristic.\n");
-                    gatt_client_discover_characteristics_for_service_by_uuid16(handle_gatt_client_event, connection_handle, &server_service, ORG_BLUETOOTH_CHARACTERISTIC_TEMPERATURE);
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case TC_W4_CHARACTERISTIC_RESULT:
-            switch (hci_event_packet_get_type(packet)) {
-                case GATT_EVENT_CHARACTERISTIC_QUERY_RESULT:
-                    printf("Storing characteristic\n");
-                    gatt_event_characteristic_query_result_get_characteristic(packet, &server_characteristic);
-                    break;
-                case GATT_EVENT_QUERY_COMPLETE:
-                    att_status = gatt_event_query_complete_get_att_status(packet);
-                    if (att_status != ATT_ERROR_SUCCESS) {
-                        printf("CHARACTERISTIC_QUERY_RESULT, ATT Error 0x%02x.\n", att_status);
-                        gap_disconnect(connection_handle);
-                        break;
-                    }
-                    // register handler for notifications
-                    listener_registered = true;
-                    gatt_client_listen_for_characteristic_value_updates(&notification_listener, handle_gatt_client_event, connection_handle, &server_characteristic);
-                    // enable notifications
-                    printf("Enable notify on characteristic.\n");
-                    state = TC_W4_ENABLE_NOTIFICATIONS_COMPLETE;
-                    gatt_client_write_client_characteristic_configuration(handle_gatt_client_event, connection_handle,
-                                                                          &server_characteristic, GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION);
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case TC_W4_ENABLE_NOTIFICATIONS_COMPLETE:
-            switch (hci_event_packet_get_type(packet)) {
-                case GATT_EVENT_QUERY_COMPLETE:
-                    printf("Notifications enabled, ATT status 0x%02x\n", gatt_event_query_complete_get_att_status(packet));
-                    if (gatt_event_query_complete_get_att_status(packet) != ATT_ERROR_SUCCESS) break;
-                    state = TC_W4_READY;
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case TC_W4_READY:
-            switch (hci_event_packet_get_type(packet)) {
-                case GATT_EVENT_NOTIFICATION: {
-                    uint16_t value_length = gatt_event_notification_get_value_length(packet);
-                    const uint8_t* value = gatt_event_notification_get_value(packet);
-                    printf("Indication value len %d\n", value_length);
-                    if (value_length == 2) {
-                        float temp = little_endian_read_16(value, 0);
-                        printf("read temp %.2f degc\n", temp / 100);
-                    } else {
-                        printf("Unexpected length %d\n", value_length);
-                    }
-                    break;
-                }
-                default:
-                    printf("Unknown packet type 0x%02x\n", hci_event_packet_get_type(packet));
-                    break;
-            }
-            break;
-        default:
-            printf("error\n");
-            break;
-    }
-}
-
-static void hci_event_handler(uint8_t packet_type, __unused uint16_t channel, __unused uint8_t* packet, uint16_t size) {
-    bd_addr_t local_addr;
-    if (packet_type != HCI_EVENT_PACKET) return;
-
-    uint8_t event_type = hci_event_packet_get_type(packet);
-    switch (event_type) {
-        case BTSTACK_EVENT_STATE:
-            if (btstack_event_state_get_state(packet) == HCI_STATE_WORKING) {
-                gap_local_bd_addr(local_addr);
-                printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
-                client_start();
-            } else {
-                state = TC_OFF;
-            }
-            break;
-        case GAP_EVENT_ADVERTISING_REPORT:
-            if (state != TC_W4_SCAN_RESULT) return;
-            // check name in advertisement
-            if (!advertisement_report_contains_device_name("DitooPro", packet)) return;
-            // store address and type
-            gap_event_advertising_report_get_address(packet, server_addr);
-            server_addr_type = gap_event_advertising_report_get_address_type(packet);
-            // stop scanning, and connect to the device
-            state = TC_W4_CONNECT;
-            gap_stop_scan();
-            printf("Connecting to device with addr %s.\n", bd_addr_to_str(server_addr));
-            gap_connect(server_addr, server_addr_type);
-            break;
-        case HCI_EVENT_LE_META:
-            // wait for connection complete
-            switch (hci_event_le_meta_get_subevent_code(packet)) {
-                case HCI_SUBEVENT_LE_CONNECTION_COMPLETE:
-                    if (state != TC_W4_CONNECT) return;
-                    connection_handle = hci_subevent_le_connection_complete_get_connection_handle(packet);
-                    // initialize gatt client context with handle, and add it to the list of active clients
-                    // query primary services
-                    printf("Search for env sensing service.\n");
-                    state = TC_W4_SERVICE_RESULT;
-                    gatt_client_discover_primary_services_by_uuid16(handle_gatt_client_event, connection_handle, ORG_BLUETOOTH_SERVICE_GENERIC_ACCESS);
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case HCI_EVENT_DISCONNECTION_COMPLETE:
-            // unregister listener
-            connection_handle = HCI_CON_HANDLE_INVALID;
-            if (listener_registered) {
-                listener_registered = false;
-                gatt_client_stop_listening_for_characteristic_value_updates(&notification_listener);
-            }
-            printf("Disconnected %s\n", bd_addr_to_str(server_addr));
-            if (state == TC_OFF) break;
-            client_start();
-            break;
-        default:
-            break;
-    }
-}
-
-static void heartbeat_handler(struct btstack_timer_source* ts) {
-    // Invert the led
-    static bool quick_flash;
-    static bool led_on = true;
-
-    led_on = !led_on;
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
-    if (listener_registered && led_on) {
-        quick_flash = !quick_flash;
-    } else if (!listener_registered) {
-        quick_flash = false;
-    }
-
-    // Restart timer
-    btstack_run_loop_set_timer(ts, (led_on || quick_flash) ? LED_QUICK_FLASH_DELAY_MS : LED_SLOW_FLASH_DELAY_MS);
-    btstack_run_loop_add_timer(ts);
 }
